cc3a8ca0d154770d67e11708b65e8f4cb8b531c6
Anjali Khandelwal <Anjali.Khandelwal@iwhalecloud.com>
2024-07-11 12:57:28 +0530

    Testing

--- backend/test/Lock.js
+++ backend/test/Lock.js
@@ -1,126 +1,71 @@
-const {
-  time,
-  loadFixture,
-} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
-const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
-const { expect } = require("chai");
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
 
-describe("Lock", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
+describe('VestingContract', function () {
+  let VestingContract, vestingContract, Token, token, owner, addr1, addr2, addr3;
 
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+  beforeEach(async function () {
+    // Get the ContractFactory and Signers here.
+    [owner, addr1, addr2, addr3] = await ethers.getSigners();
 
-    // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
+    // Deploy the ERC20 token
+    Token = await ethers.getContractFactory('MintableToken');
+    token = await Token.deploy('TestToken', 'TTK', 1000000);
+    await token.deployed();
 
-    const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    // Deploy the VestingContract
+    VestingContract = await ethers.getContractFactory('VestingContract');
+    vestingContract = await VestingContract.deploy(token.address);
+    await vestingContract.deployed();
 
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
-  }
-
-  describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.unlockTime()).to.equal(unlockTime);
-    });
-
-    it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.owner()).to.equal(owner.address);
-    });
-
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
-
-      expect(await ethers.provider.getBalance(lock.target)).to.equal(
-        lockedAmount
-      );
-    });
-
-    it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future"
-      );
-    });
+    // Transfer some tokens to the vesting contract
+    await token.transfer(vestingContract.address, 500000);
   });
 
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called too soon", async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
+  it('should allow the admin to create a vesting schedule', async function () {
+    await vestingContract.setRole(addr1.address, 0); // Role.User
+    await vestingContract.addBeneficiary(addr1.address, 250000);
 
-        await expect(lock.withdraw()).to.be.revertedWith(
-          "You can't withdraw yet"
-        );
-      });
+    const schedule = await vestingContract.vestingSchedules(addr1.address);
+    expect(schedule.totalAmount).to.equal(250000);
+  });
 
-      it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
+  it('should release tokens properly after cliff period', async function () {
+    await vestingContract.setRole(addr1.address, 0); // Role.User
+    await vestingContract.addBeneficiary(addr1.address, 250000);
+    await vestingContract.startVesting();
 
-        // We can increase the time in Hardhat Network
-        await time.increaseTo(unlockTime);
+    // Fast forward time past the cliff period (10 months)
+    await ethers.provider.send('evm_increaseTime', [10 * 30 * 24 * 60 * 60]);
+    await ethers.provider.send('evm_mine', []);
 
-        // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-          "You aren't the owner"
-        );
-      });
+    await vestingContract.connect(addr1).releaseTokens();
+    const balance = await token.balanceOf(addr1.address);
+    expect(balance).to.be.gt(0);
+  });
 
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture
-        );
+  it('should correctly calculate releasable amount based on duration', async function () {
+    await vestingContract.setRole(addr1.address, 0); // Role.User
+    await vestingContract.addBeneficiary(addr1.address, 250000);
+    await vestingContract.startVesting();
 
-        // Transactions are sent using the first signer by default
-        await time.increaseTo(unlockTime);
+    // Fast forward time by 1 year
+    await ethers.provider.send('evm_increaseTime', [365 * 24 * 60 * 60]);
+    await ethers.provider.send('evm_mine', []);
 
-        await expect(lock.withdraw()).not.to.be.reverted;
-      });
-    });
+    const releasable = await vestingContract.releasableAmount(addr1.address);
+    expect(releasable).to.be.closeTo(125000, 1); // Approximately half of the total amount
+  });
 
-    describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture
-        );
+  it('should prevent token release before cliff period', async function () {
+    await vestingContract.setRole(addr1.address, 0); // Role.User
+    await vestingContract.addBeneficiary(addr1.address, 250000);
+    await vestingContract.startVesting();
 
-        await time.increaseTo(unlockTime);
+    await expect(vestingContract.connect(addr1).releaseTokens()).to.be.revertedWith('Cliff period not reached');
+  });
 
-        await expect(lock.withdraw())
-          .to.emit(lock, "Withdrawal")
-          .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-      });
-    });
-
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
-          [lockedAmount, -lockedAmount]
-        );
-      });
-    });
+  it('should only allow admin to add beneficiaries', async function () {
+    await expect(vestingContract.connect(addr2).addBeneficiary(addr3.address, 250000)).to.be.revertedWith('Ownable: caller is not the owner');
   });
 });
